bf9604956261168c1ec83dff76ec17016512d514
anii02092006 <anithaanitha41025@gmail.com>
2025-11-21 21:35:00 +0530

    Create program24.c
    find largest in array

---
+++ C/program24.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int n, arr[100], max, i;
+    scanf("%d", &n);
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    max = arr[0];
+    for(i = 1; i < n; i++)
+        if(arr[i] > max)
+            max = arr[i];
+    printf("%d", max);
+    return 0;
+}
