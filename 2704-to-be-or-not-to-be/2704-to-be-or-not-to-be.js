96d2a991dca8b4db064a6f363597f27c7ef9b8fb
anii02092006 <anithaanitha41025@gmail.com>
2025-12-06 11:17:14 +0530

    Time: 57 ms (6.26%), Space: 54.3 MB (31.69%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,12 @@
+function expect(val) {
+    return {
+        toBe: function(other) {
+            if (val === other) return true;
+            else throw new Error("Not Equal");
+        },
+        notToBe: function(other) {
+            if (val !== other) return true;
+            else throw new Error("Equal");
+        }
+    };
+}
